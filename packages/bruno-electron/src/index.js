2d3b8f46b2d7c4db6c7b33f6db2746713b12e93b
helloworldhc <yeyezizhi@outlook.com>
2026-08-18 15:56:26 +0800

    chore: remove Golden Edition and telemetry tracking

--- packages/bruno-electron/src/index.js
+++ packages/bruno-electron/src/index.js
@@ -76,7 +76,7 @@ const apiSpecWatcher = new ApiSpecWatcher();
 // Reference: https://content-security-policy.com/
 const contentSecurityPolicy = [
   'default-src \'self\'',
-  'connect-src \'self\' https://*.posthog.com',
+  'connect-src \'self\'',
   'font-src \'self\' https: data:;',
   'frame-src data:',
   'script-src \'self\' data:',
